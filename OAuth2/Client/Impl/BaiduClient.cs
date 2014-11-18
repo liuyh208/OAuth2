6e3fec29990ef1cf0889976e57dc7f97903ee968
liuyh208 <liuyh208@sohu.com>
2014-11-18 15:23:09 +0800

    增加百度、新浪微博登录

---
+++ OAuth2/Client/Impl/BaiduClient.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using OAuth2.Configuration;
+using OAuth2.Infrastructure;
+using OAuth2.Models;
+
+namespace OAuth2.Client.Impl
+{
+    /// <summary>
+    /// baidu  OAuth2
+    /// </summary>
+    public  class BaiduClient : OAuth2Client
+    {
+        public BaiduClient(IRequestFactory factory, IClientConfiguration configuration)
+            : base(factory, configuration)
+        {
+        }
+        public override string Name
+        {
+            get { return "Baidu"; }
+        }
+
+        protected override Endpoint AccessCodeServiceEndpoint
+        {
+            get
+            {
+                return new Endpoint
+                {
+                    BaseUri = "https://openapi.baidu.com",
+                    Resource = "/oauth/2.0/authorize"
+                };
+            }
+        }
+
+        protected override Endpoint AccessTokenServiceEndpoint
+        {
+            get
+            {
+                return new Endpoint
+                {
+                    BaseUri = "https://openapi.baidu.com",
+                    Resource = "/oauth/2.0/token"
+                };
+            }
+        }
+
+        protected override Endpoint UserInfoServiceEndpoint
+        {
+            get
+            {
+                //https://openapi.baidu.com/rest/2.0/passport/users/getLoggedInUser
+                return new Endpoint
+                {
+                    BaseUri = "https://openapi.baidu.com",
+                    Resource = "/rest/2.0/passport/users/getInfo"
+                };
+            }
+        }
+
+        protected override Models.UserInfo ParseUserInfo(string content)
+        {
+            var cnt = JObject.Parse(content);
+            var names = cnt["username"].SafeGet(t => t.Value<string>());
+            var portrait = cnt["portrait"].SafeGet(t => t.Value<string>());
+            var result = new UserInfo
+            {
+
+                ProviderName = this.Name,
+                Id = cnt["userid"].Value<int>().ToString(),
+                LastName =  cnt["name"].SafeGet(t => t.Value<string>()),
+                AvatarUri =
+                {
+                    Small = string.Format("http://tb.himg.baidu.com/sys/portraitn/item/{0}", portrait),
+                    Normal = string.Format("http://tb.himg.baidu.com/sys/portraitn/item/{0}", portrait),
+                    Large = string.Format("http://tb.himg.baidu.com/sys/portrait/item/{0}",portrait)
+                }
+            };
+            return result;
+        }
+    }
+}
---
+++ OAuth2/Client/Impl/WeiBoClient.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using OAuth2.Configuration;
+using OAuth2.Infrastructure;
+using OAuth2.Models;
+
+namespace OAuth2.Client.Impl
+{
+    /// <summary>
+    /// sina Weibo OAuth
+    /// </summary>
+    class WeiboClient : OAuth2Client
+    {
+        public WeiboClient(IRequestFactory factory, IClientConfiguration configuration)
+            : base(factory, configuration)
+        {
+        }
+        public override string Name
+        {
+            get { return "Weibo"; }
+        }
+
+        protected override Endpoint AccessCodeServiceEndpoint
+        {
+            get
+            {
+                return new Endpoint
+                {
+                    BaseUri = "https://api.weibo.com",
+                    Resource = "/oauth2/authorize"
+                };
+            }
+        }
+
+        protected override Endpoint AccessTokenServiceEndpoint
+        {
+            get
+            {
+                return new Endpoint
+                {
+                    BaseUri = "https://api.weibo.com",
+                    Resource = "/oauth2/access_token"
+                };
+            }
+        }
+
+        protected override Endpoint UserInfoServiceEndpoint
+        {
+            get
+            {
+                //https://api.weibo.com/2/users/domain_show.json
+                return new Endpoint
+                {
+                    BaseUri = "https://api.weibo.com",
+                    Resource = "/2/users/domain_show.json"
+                };
+            }
+        }
+
+        protected override Models.UserInfo ParseUserInfo(string content)
+        {
+            var cnt = JObject.Parse(content);
+            var names = cnt["name"].SafeGet(t => t.Value<string>());
+            
+            var result = new UserInfo
+            {
+
+                ProviderName = this.Name,
+                Id = cnt["id"].Value<string>(),
+                LastName =  cnt["name"].SafeGet(t => t.Value<string>()),
+                AvatarUri =
+                {
+                    Small =string.Empty,
+                    Normal = cnt["profile_image_url"].SafeGet(t => t.Value<string>()),
+                    Large = cnt["avatar_large"].SafeGet(t => t.Value<string>())
+                }
+            };
+            return result;
+        }
+    }
+}
